435c4b895e535508e998d6f0d475928629c6c21e
YiyuanZhao <55082427+YiyuanZhao@users.noreply.github.com>
2020-12-23 15:19:51 +0800

    read hopping matrix

--- src/monolayerModel.f90
+++ src/monolayerModel.f90
@@ -1,14 +1,17 @@
 Module Parameter
     real *8, dimension(:, :), allocatable :: kpointsRecip;
+    real *8, dimension(:), allocatable :: hoppingPara, delta;
     integer, allocatable :: hoppingMatrix(:, :);
+    integer atomNumber;
 end Module Parameter
 
 Program monolayerModel
     use Parameter
-    implicit none    
+    implicit none       
     call readKpointsRecip();
     call readHoppingMatrix();
-    Call destructor();
+    call readHoppingParameter();
+    call destructor();
 end Program monolayerModel
 
 subroutine readKpointsRecip()
@@ -27,8 +30,9 @@ end subroutine readKpointsRecip
 
 subroutine readHoppingMatrix()
     use Parameter
-    integer readLength, numIdx
+    integer readLength, numIdx;
     integer readStatus;
+    atomNumber = 0;
     numIdx = 1;
     open(20, file = '../data/hoppingMat.dat');
     read(20, *) readLength;
@@ -39,13 +43,51 @@ subroutine readHoppingMatrix()
         if (readStatus /= 0) then
             exit
         end if
+        if (hoppingMatrix(6, numIdx) > atomNumber) then
+            atomNumber =  hoppingMatrix(6, numIdx)
+        end if
         numIdx = numIdx + 1;        
     end do
     close(20)
 end subroutine readHoppingMatrix
 
+subroutine readHoppingParameter()
+    use Parameter
+    implicit none
+    Integer readLine, numIdx;
+    integer readStatus;
+    open (30, file = '../data/hoppingPara.dat');
+    read(30, *) readLine;
+    allocate(hoppingPara(readLine - 1));
+    allocate(delta(atomNumber));
+    select case (atomNumber)
+    case (1)
+        read(30, *) delta(1);
+    case (2)
+        read(30, *) delta(1), delta(2);
+    case (3)
+        read(30, *) delta(1), delta(2), delta(3);
+    case (4)
+        read(30, *) delta(1), delta(2), delta(3), delta(4);
+    case default
+        write(*, *) "atom exceeded, change origin Code";
+        stop
+    end select
+    numIdx = 1;
+    do
+        read(30, *,iostat = readStatus) hoppingPara(numIdx);
+        if (readStatus /= 0) then
+            exit
+        end if
+        numIdx = numIdx + 1;
+    end do
+
+    close(30);
+end subroutine readHoppingParameter
+
 subroutine destructor()
     use Parameter
     deallocate(kpointsRecip);
     deallocate(hoppingMatrix);
+    deallocate(delta);
 end subroutine destructor
